8dedb0b90ee61b1657e1925635836ca4d03ee78b
password_stars <dajin.kim@my.jcu.edu.au>
2025-06-08 14:22:35 +0800

    Update index.html(HomePage), register page. Add terms and condition, payment, contact web page sections.

--- Front-end /script.js
+++ Front-end /script.js
@@ -1,7 +1,7 @@
 function login() {
-    alert("Go to the log in page.");
-  }
-  
-  function register() {
-    alert("Go to the registration page.");
-  }
+  alert("Go to the log in page.");
+}
+
+function register() {
+  alert("Go to the registration page.");
+}
